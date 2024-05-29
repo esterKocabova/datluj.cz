5c6e4391f92559ad212ecbf2f191beb472853d6c
Ester <kocabova@mamiart.cz>
2024-05-29 13:07:25 +0200

    fáze 1

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './style.css';
-
 interface IWordboxProp {
   word: string;
 }
-
-const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
-  
+const Wordbox: React.FC<IWordboxProp> = ({ word }) => {
+  const [lettersLeft, setLettersLeft] = useState<string>(word);
+  const handleKeyUp = useCallback((event: KeyboardEvent) => {
+    setLettersLeft((prevLettersLeft) => {
+      if (prevLettersLeft.length === 0) {
+        return prevLettersLeft;
+      }
+      if (event.key === prevLettersLeft[0]) {
+        return prevLettersLeft.slice(1);
+      }
+      return prevLettersLeft;
+    });
+  }, []);
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [handleKeyUp]);
   return (
     <div className="wordbox">{lettersLeft}</div>
   );
 };
-
 export default Wordbox;
